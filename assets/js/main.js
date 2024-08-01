c266ab251c727c83e4d568d84b513d7a1d76a1e6
J0YALJ0HN <JoyalJohn3500@gmail.com>
2024-08-01 11:34:04 +0100

    finished MAIN and HOME as well as added blurred header

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
