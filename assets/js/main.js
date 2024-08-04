2716379d6c3e550fc7e744da12d8e01b236743b7
J0YALJ0HN <JoyalJohn3500@gmail.com>
2024-08-04 17:37:59 +0100

    completed and tested email contact form html+css finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,33 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+   e.preventDefault()
+   
+   // serviceID - templateID - #form - publicKey
+   emailjs.sendForm('service_nqms6ho', 'template_weav1eb', '#contact-form', 'qdccEVZg3gdymPHMN')
+      .then(() => {
+         // Show sent message
+         contactMessage.textContent = 'Message sent successfully '
+
+         // Remove message after five seconds
+         setTimeout(() => {
+            contactMessage.textContent = ''
+         }, 5000)
+
+         // Clear input fields
+         contactForm.reset()
+
+      }, () => {
+         // Show error message
+         contactMessage.textContent = 'Message not sent (service error) '
+      })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
