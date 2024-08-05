d6976154c04dfcb7b8d8c90a43afe7c0960d22f9
J0YALJ0HN <JoyalJohn3500@gmail.com>
2024-08-05 13:24:52 +0100

    add smooth scroll animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,17 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+    // reset:true // Animations repeat
+})
+
+sr.reveal('.home__data, .experience, .skills, .contact__container')
+sr.reveal('.home__img', {delay: 600})
+sr.reveal('.home__scroll', {delay: 800})
+sr.reveal('.work__card, .services__card', {interval: 100})
+sr.reveal('.about__content', {origin: 'right'})
+sr.reveal('.about__img', {origin: 'left'})
